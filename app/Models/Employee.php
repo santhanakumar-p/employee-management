96894f867bdae819605e4b402039fd62cda6b80c
Santhanakumar P <santhanakumar99@outlook.com>
2026-07-07 07:46:37 +0530

    Add Employee model, migration, and seeder for employee data

---
+++ app/Models/Employee.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'department_id',
+        'name',
+        'email',
+    ];
+}
